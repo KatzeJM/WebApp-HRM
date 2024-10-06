9fa4b6f3433266d85f5ded4450195c7efc1ef4cf
Jonathan <katzenjn@gmail.com>
2024-10-06 08:07:00 -0600

    DB Migration, Added tables departamentos and puestos, update empleados

--- backend-hrm/app.py
+++ backend-hrm/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db
+from schemas import db
 
 
 app = Flask(__name__)
--- backend-hrm/schemas.py
+++ backend-hrm/schemas.py
@@ -2,11 +2,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Employee(db.Model):
-    __tablename__ = 'empleado'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    apellido = db.Column(db.String(100), nullable=False)
-    direccion = db.Column(db.String(250), nullable=False)
+class Empleado(db.Model):
+    __tablename__ = 'empleados'
+    id_empleado = db.Column(db.Integer, primary_key=True)
+    estado = db.Column(db.Boolean) 
+    nombre = db.Column(db.String(200), nullable=False)
+    apellido = db.Column(db.String(200), nullable=False)
+    direccion = db.Column(db.String(300), nullable=False)
+    fechaNacimiento = db.Column(db.Date, nullable=False)
+   #Foreing Keys 
+    departamento_id = db.Column(db.Integer, db.ForeignKey('departamentos.id_departamento'), nullable=False)
+    puesto_id = db.Column(db.Integer, db.ForeignKey('puestos.id_puesto'), nullable=False)  
+    #Referencia
+    departamento = db.relationship('Departamento', backref='empleados')
+    puesto = db.relationship('Puesto', backref='empleados')
 
 
+class Departamento(db.Model):
+    __tablename__ = 'departamentos'
+    
+    id_departamento = db.Column(db.Integer, primary_key=True)
+    departamento = db.Column(db.String(200), nullable=False)
+    estado = db.Column(db.Boolean) 
+
+
+class Puesto(db.Model):
+    __tablename__ = 'puestos'
+    
+    id_puesto    = db.Column(db.Integer, primary_key=True)
+    puesto = db.Column(db.String(200), nullable=False)
+    estado = db.Column(db.Boolean) 
